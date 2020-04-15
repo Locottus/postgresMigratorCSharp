d497bfbdb51f810d1f0d7e52ad2f779613145f64
herlich steven gonzalez zambrano <herlich@gmail.com>
2020-04-15 11:33:55 -0600

    first commit

---
+++ cargarDatosCSVPostgres/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace cargarDatosCSVPostgres
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtQueries = new System.Windows.Forms.RichTextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtTabla = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.txtCreate = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(-100, -226);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(139, 54);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(72, 53);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(52, 55);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.Text = ",";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(35, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(129, 17);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "caracter separador";
+            // 
+            // txtQueries
+            // 
+            this.txtQueries.Location = new System.Drawing.Point(31, 129);
+            this.txtQueries.Margin = new System.Windows.Forms.Padding(4);
+            this.txtQueries.Name = "txtQueries";
+            this.txtQueries.Size = new System.Drawing.Size(765, 363);
+            this.txtQueries.TabIndex = 4;
+            this.txtQueries.Text = "";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(181, 30);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(116, 17);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "nombre de Tabla";
+            // 
+            // txtTabla
+            // 
+            this.txtTabla.Location = new System.Drawing.Point(185, 55);
+            this.txtTabla.Margin = new System.Windows.Forms.Padding(4);
+            this.txtTabla.Name = "txtTabla";
+            this.txtTabla.Size = new System.Drawing.Size(221, 22);
+            this.txtTabla.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(668, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(118, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "abrir archivo CSV";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(668, 55);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(117, 37);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "abrir";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // txtCreate
+            // 
+            this.txtCreate.Location = new System.Drawing.Point(31, 512);
+            this.txtCreate.Margin = new System.Windows.Forms.Padding(4);
+            this.txtCreate.Name = "txtCreate";
+            this.txtCreate.Size = new System.Drawing.Size(765, 97);
+            this.txtCreate.TabIndex = 5;
+            this.txtCreate.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(823, 638);
+            this.Controls.Add(this.txtCreate);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.txtTabla);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtQueries);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "data parser";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox txtQueries;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtTabla;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RichTextBox txtCreate;
+    }
+}
+
---
+++ cargarDatosCSVPostgres/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace cargarDatosCSVPostgres
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        public string createScript(string[] campos)
+        {
+            string lista = "create table public." + txtTabla.Text + " ( \n" +
+                "id SERIAL PRIMARY KEY,\n";
+            for (int i = 0; i < campos.Length; i++)
+            {
+                lista = lista + campos[i].Replace("'", "") + " text null ,\n";
+            }
+            lista = lista.Remove(lista.Length - 2);
+            return  lista + " );\n ";
+        }
+        public string encabezado()
+        {
+            return "insert into " + txtTabla.Text + " " ;
+        }
+
+        public string campos(string[] campos)
+        {
+            //campos de encabezado
+            string lista = "";
+            for(int i =0; i < campos.Length; i++)
+            {
+                lista = lista +   campos[i].Replace("'","") + ",";
+            }
+            lista = lista.Remove(lista.Length - 1);
+            return encabezado() + " ( " + lista + " ) ";
+        }
+
+        public string valores(string[] campos)
+        {
+            string lista = "";
+            for (int i = 0; i < campos.Length; i++)
+            {
+                lista = lista + campos[i] + ",";
+            }
+            lista = lista.Remove(lista.Length - 1);
+            return " values ( " + lista + " ); ";
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private string modificaStr(string token)
+        {
+            
+            if (token.IndexOf('\'') > 0) {
+                token = token.Replace("'", "''");
+            }
+            return "'" + token + "'";
+        }
+        private void cargaCSV(string archivo)
+        {
+            using (var reader = new StreamReader(archivo, Encoding.GetEncoding("iso-8859-15")))
+            {
+                int c = 0;
+                string encabezado = null;
+                List<string[]> listA = new List<string[]>();
+                List<string[]> listB = new List<string[]>();
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    if (line.Length > 0)
+                    {
+                        
+                        var values = line.Split(Char.Parse(textBox2.Text.ToString()));
+                        
+                        for(int i = 0; i < values.Length; i++)
+                        {
+                            values[i] = modificaStr(values[i]);
+                            Console.WriteLine(values[i]);
+                        }
+                        
+                        if (c == 0)
+                        {
+                            listA.Add(values);
+                            encabezado = campos(values);
+                            string cStr = createScript(values);
+                            Console.WriteLine(cStr);
+                            txtCreate.AppendText(cStr);
+
+                        }
+
+                        else
+                        {
+                            listB.Add(values);
+                            txtQueries.AppendText(encabezado + valores(values) + "\n");
+                        }
+                        c++;
+                        
+                    }
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.FileName = "";
+            openFileDialog1.ShowDialog();
+            string Archivo = openFileDialog1.FileName;
+            if (Archivo.Length > 0)
+                cargaCSV(Archivo);
+            else
+                MessageBox.Show("ingrese nombre de la tabla y delimitador de archivo csv");
+        }
+    }
+}
